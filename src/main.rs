f7ee9892c0c5cc50513521efb45e2c1e872f5cbf
shota-tsuji <shotatuzi@gmail.com>
2022-12-17 18:05:08 +0900

    chore: print v_x

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,7 @@ use plotlib::style::{LineJoin, LineStyle, PointMarker, PointStyle};
 use plotlib::view::ContinuousView;
 
 fn main() {
-    let mat1 = arr2(&[
-        [4., -17.],
-        [15., -4.],
-        [30., -7.],
-        [100., 50.],
-        [200., 70.],
-    ]);
+    let mat1 = arr2(&[[4., -17.], [15., -4.], [30., -7.], [100., 50.], [200., 70.]]);
 
     let (v_x, v_y) = mat1.view().split_at(Axis(1), 1);
     let a_11 = v_x.mapv(|v: f64| v.powf(2.)).sum();
@@ -22,10 +16,7 @@ fn main() {
     let b_1 = v_x.iter().zip(v_y.iter()).map(|(x, y)| x * y).sum();
     let b_2 = v_y.sum();
 
-    let mat_a: Array2<f64> = arr2(&[
-        [a_11, a_12],
-        [a_12, a_22],
-    ]);
+    let mat_a: Array2<f64> = arr2(&[[a_11, a_12], [a_12, a_22]]);
     println!("{}", mat_a);
 
     let v_b = arr1(&[b_1, b_2]);
@@ -34,27 +25,29 @@ fn main() {
     let coefficient_x = mat_a.solve(&v_b).unwrap();
     println!("{}", coefficient_x);
 
-    let precision_y = v_x.map(|&x| f(coefficient_x[0], coefficient_x[1], x)).into_raw_vec();
+    let precision_y = v_x
+        .map(|&x| f(coefficient_x[0], coefficient_x[1], x))
+        .into_raw_vec();
 
     let mut data1 = Vec::new();
     Zip::from(v_x)
         .and(v_y)
         .for_each(|&x, &y| data1.push((x, y)));
 
-    let s1: Plot = Plot::new(data1).point_style(
-        PointStyle::new().colour("#35C788")
-    );
+    let s1: Plot = Plot::new(data1).point_style(PointStyle::new().colour("#35C788"));
 
     let mut precision = Vec::new();
     let tmp = v_x.to_shape(((v_x.len()), Order::RowMajor)).unwrap();
-    let v_x =  tmp.view();
+    let v_x = tmp.view();
+    println!("{}", v_x);
+
     Zip::from(v_x)
         .and(&precision_y)
         .for_each(|&x, &y| precision.push((x, y)));
     let l1 = Plot::new(precision).line_style(
         LineStyle::new()
             .colour("burlywood")
-            .linejoin(LineJoin::Round)
+            .linejoin(LineJoin::Round),
     );
 
     let v = ContinuousView::new()
