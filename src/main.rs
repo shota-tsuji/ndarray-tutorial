5a9fb40275215de2e55587b971a069e0a05bc9bc
shota-tsuji <shotatuzi@gmail.com>
2022-12-17 14:47:57 +0900

    feat: plot sample

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use ndarray::{arr1, arr2, array, Array1, Array2, Axis};
 use ndarray_linalg::{error, random, Solve};
+use plotlib::page::Page;
+use plotlib::repr::Plot;
+use plotlib::style::{PointMarker, PointStyle};
+use plotlib::view::ContinuousView;
 
 fn main() {
     let mat1 = arr2(&[
@@ -30,4 +34,33 @@ fn main() {
     let x = mat_a.solve(&v_b).unwrap();
     println!("{}", x);
 
+    let data1 = [
+        (-3.0, 2.3),
+        (-1.6, 5.3),
+        (0.3, 0.7),
+        (4.3, -1.4),
+        (6.4, 4.3),
+        (8.5, 3.7),
+    ];
+
+    let s1: Plot = Plot::new(data1).point_style(
+        PointStyle::new()
+            .marker(PointMarker::Square)
+            .colour("#DD3355"),
+    );
+
+    let data2 = vec![(-1.4, 2.5), (7.2, -0.3)];
+    let s2: Plot = Plot::new(data2).point_style(
+        PointStyle::new().colour("#35C788")
+    );
+
+    let v = ContinuousView::new()
+        .add(s1)
+        .add(s2)
+        .x_range(-5., 10.)
+        .y_range(-2., 6.)
+        .x_label("x label")
+        .y_label("y label");
+
+    Page::single(&v).save("scatter.svg").unwrap();
 }
